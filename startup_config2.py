bab0292461cc317c227b61a205be1e5bdcc1140e
Jaschluckbier <jack.schluckbier@arista.com>
2025-08-19 17:11:39 -0500

    update bootfile2

--- startup_config2.py
+++ startup_config2.py
@@ -7,12 +7,12 @@ import hashlib
 import sys
 
 # Install and boot system (only if checksum matches)
-os.system(f"FastCli -p 15 -c 'configure terminal')
-os.system(f"FastCli -p 15 -c 'interface management 1')
-os.system(f"FastCli -p 15 -c 'ip address dhcp')
-os.system(f"FastCli -p 15 -c 'no shut')
-os.system(f"FastCli -p 15 -c 'ip route 0.0.0.0/0 192.168.5.1')
-os.system(f"FastCli -p 15 -c 'write memory')
+os.system(f"FastCli -p 15 -c 'configure terminal'")
+os.system(f"FastCli -p 15 -c 'interface management 1'")
+os.system(f"FastCli -p 15 -c 'ip address dhcp'")
+os.system(f"FastCli -p 15 -c 'no shut'")
+os.system(f"FastCli -p 15 -c 'ip route 0.0.0.0/0 192.168.5.1'")
+os.system(f"FastCli -p 15 -c 'write memory'")
 
 os.system(f"FastCli -p 15 -c 'copy tftp://192.168.4.199/vEOS-lab-4.34.2F.swi flash:'")
 os.system(f"FastCli -p 15 -c 'install image flash:vEOS-lab-4.34.2F.swi'")
